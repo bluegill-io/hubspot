f1a60d7ffbbf0207ece90a9cf70b79496a943945
65monroe <65monroe@gmail.com>
2016-05-19 08:30:42 -0400

    Update seed.rb

--- db/seed.rb
+++ db/seed.rb
@@ -1,13 +1,10 @@
 DealStage.create({uuid: "8b7a49c3-f7de-422d-8cc3-acc3b656f748", stage_name: "Project Mentioned"})
+DealStage.create({uuid: "67fb3c7e-154b-43b9-b528-9a489084554b", stage_name: "Progress Drawings Issued"})
+DealStage.create({uuid: "629fa075-92b4-46af-b886-fa843aa4dc23", stage_name: "Budget Submitted"})
+DealStage.create({uuid: "7f75c699-0140-4a5f-be87-67b38fa16764", stage_name: "Invited to Bid"})
+DealStage.create({uuid: "", stage_name: "Closed Not Intrested"})
 DealStage.create({uuid: "8b7a49c3-f7de-422d-8cc3-acc3b656f748", stage_name: "Project Mentioned"})
-DealStage.create({uuid: "8b7a49c3-f7de-422d-8cc3-acc3b656f748", stage_name: "Project Mentioned"})
-DealStage.create({uuid: "8b7a49c3-f7de-422d-8cc3-acc3b656f748", stage_name: "Project Mentioned"})
-
-#If Value = "67fb3c7e-154b-43b9-b528-9a489084554b"  puts  "Progress Drawings Issued"
-#If Value = "629fa075-92b4-46af-b886-fa843aa4dc23"  puts  "Budget Submitted"
-#If Value = "7f75c699-0140-4a5f-be87-67b38fa16764"  puts "Invited to Bid"
-#If Value = ""  puts   "Closed Not Intrested"
-#If Value = "c801e343-f3f6-4a68-ba0a-6b5b8f10ab3d"puts "Actively Bidding"
-#If Value = "f3eb20a4-7aed-4577-8e97-66e78d7d5203"  puts   "Bid Submitted"
-#If Value = "d331deb8-d9e0-445c-beb7-7bea23bb63af"  puts "Closed Won"
-#If Value = "95dca5e8-d556-460c-932b-4385fabfa562"  puts "Closed Lost",
+DealStage.create({uuid: "c801e343-f3f6-4a68-ba0a-6b5b8f10ab3d", stage_name: "Actively Bidding"})
+DealStage.create({uuid: "f3eb20a4-7aed-4577-8e97-66e78d7d5203", stage_name: "Bid Submitted"})
+DealStage.create({uuid: "d331deb8-d9e0-445c-beb7-7bea23bb63af", stage_name: "Closed Won"})
+DealStage.create({uuid: "95dca5e8-d556-460c-932b-4385fabfa562", stage_name: "Closed Lost"})
