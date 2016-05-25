4b1d548b46bf5eb052cbc92b580e780ce32ec67b
Drew Monroe <dvmonroe6@gmail.com>
2016-05-24 22:58:17 -0500

    uncomment rub.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 require './config'
 
-# rake = Rake.application
-# rake.init
-# rake.load_rakefile
+rake = Rake.application
+rake.init
+rake.load_rakefile
 
-# rake['db:drop'].invoke()
-# rake['db:create'].invoke()
-# rake['db:migrate'].invoke()
-# rake['db:seed'].invoke()
+rake['db:drop'].invoke()
+rake['db:create'].invoke()
+rake['db:migrate'].invoke()
+rake['db:seed'].invoke()
 
 
-# Api::Companies.new(ENV['COMPANY_URL']).retreive
-# puts "Companies Done"
-# Api::Contacts.new(ENV['CONTACT_URL']).retreive
-# puts "Contacts Done"
-# Api::Deals.new(ENV['DEAL_URL']).retreive
-# puts "Deals Done"
-# Api::Engagements.new(ENV['ENGAGEMENT_URL']).retreive
-# puts "Engagements Done"
-# Api::Owners.new(ENV['OWNER_URL']).retreive
-# puts "Owners Done"
+Api::Companies.new(ENV['COMPANY_URL']).retreive
+puts "Companies Done"
+Api::Contacts.new(ENV['CONTACT_URL']).retreive
+puts "Contacts Done"
+Api::Deals.new(ENV['DEAL_URL']).retreive
+puts "Deals Done"
+Api::Engagements.new(ENV['ENGAGEMENT_URL']).retreive
+puts "Engagements Done"
+Api::Owners.new(ENV['OWNER_URL']).retreive
+puts "Owners Done"
 
 
 ## create excel spreadsheet
