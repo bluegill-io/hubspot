23e3c50a95fe5ac6a63e1fcc6e74fc2c2914eca9
Drew Monroe <dvmonroe6@gmail.com>
2016-06-23 15:20:48 -0500

    update whenever scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,4 @@
-set :output, "/var/www/hubspot/logs/cron_log.log"
-
-every :day, :at => '3:40 pm' do
-  command "RAILS_ENV=production bin/run"
+# this will run 1 pm UTC
+every :day, :at => '1:00 pm' do
+  command 'cd /var/www/hubspot/current && RAILS_ENV=production bin/run'
 end
